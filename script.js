f4e26678db940e170f0cbb6922f54e633a49d458
Ivan Krug <139096035+IvanKrug@users.noreply.github.com>
2024-07-11 17:06:58 -0400

    nuevos estilos y actulizacion de sript

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let url = 'https://random-word-api.herokuapp.com/word?length=5&lang=es'
 let PALABRA;
 
-fetch('https://random-word-api.herokuapp.com/word?length=5&lang=es') 
+fetch(url) 
 .then((res) => res.json())
 .then((res) => { 
-   
   PALABRA = res[0].toUpperCase();
-    console.log(PALABRA);
-  })
-  
+  console.log(PALABRA);
+})
+
 let cantIntentos = 6;
-console.log(PALABRA);
+document.getElementById("attempts-remaining").innerText = `Intentos restantes: ${cantIntentos}`;
 
 const BUTTON = document.getElementById("guess-button");
 const GRID = document.getElementById("grid");
@@ -20,7 +19,7 @@ BUTTON.addEventListener("click", () => {
   if (INTENTO.length < 5) {
     terminar("<h2>La palabra debe contener 5 letras</h2>");
   } else {
-    console.log(PALABRA)
+    console.log(PALABRA);
     let row = document.createElement("div");
     row.className = "row";
     for (let i in PALABRA) {
@@ -36,26 +35,23 @@ BUTTON.addEventListener("click", () => {
       }
     }
     GRID.appendChild(row);
-    if (INTENTO == PALABRA) {
+    if (INTENTO === PALABRA) {
       BUTTON.disabled = true;
-     
       terminar(
         '<h2>Has acertado!</h2><button class="btnReiniciar" onclick="location.reload()">Jugar de Nuevo</button>'
       );
     } else {
       cantIntentos--;
+      document.getElementById("attempts-remaining").innerText = `Intentos restantes: ${cantIntentos}`;
     }
-  
-    if (cantIntentos == 0) {
+
+    if (cantIntentos === 0) {
       BUTTON.disabled = true;
-   
       terminar(
         '<h2>Perdiste</h2><button class="btnReiniciar" onclick="location.reload()">Jugar de Nuevo</button>'
       );
     }
   }
-
-
 });
 
 function leerIntento() {
